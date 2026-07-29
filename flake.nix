2bd50ed716070a9a521a69f3a91176d121833c1b
f1sh <f1sh@mac.local>
2026-07-29 12:00:52 +0200

    chore: update flake description and add devshell support for multiple systems

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "42arena";
+  description = "ft_lgtm";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
@@ -9,29 +9,42 @@
     };
   };
 
-  outputs = { self, nixpkgs, fenix }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    rust_toolchain = fenix.packages.${system}.stable.withComponents [
-      "cargo"
-      "rustc"
-      "rust-analyzer"
-      "rust-src"
-      "rustfmt"
-      "clippy"
-    ];
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        rust_toolchain
-        just
+  outputs = { self, nixpkgs, fenix }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
-      shellHook = ''
-        echo "entering development shell"
-      '';
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-      # LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";
+          rust_toolchain =
+            fenix.packages.${system}.stable.withComponents [
+              "cargo"
+              "rustc"
+              "rust-analyzer"
+              "rust-src"
+              "rustfmt"
+              "clippy"
+            ];
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rust_toolchain
+              just
+            ];
+
+            shellHook = ''
+              echo "entering development shell"
+            '';
+          };
+        });
     };
-  };
 }
