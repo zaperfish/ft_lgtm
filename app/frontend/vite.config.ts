03ec01c178b0824c06242602825ea62199c24ae7
zaperfish <zaperfish@gmail.com>
2026-08-02 16:18:08 +0200

    chore: add vite dev server config for api path proxies

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -16,5 +16,14 @@ export default defineConfig({
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		})
-	]
+	],
+
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:11000',
+				changeOrigin: true
+			}
+		}
+	}
 });
