8ae3cfb206af2d048de3f1e8a164d525997d47ff
zaperfish <zaperfish@gmail.com>
2026-08-05 21:44:32 +0200

    fix: properly export otel log signals

--- app/backend/src/telemetry/mod.rs
+++ app/backend/src/telemetry/mod.rs
@@ -4,6 +4,7 @@ pub mod traces;
 
 use anyhow::Result;
 use opentelemetry::trace::TracerProvider;
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
 use opentelemetry_sdk::Resource;
 use tracing_subscriber::{EnvFilter, prelude::*};
 
@@ -14,8 +15,8 @@ pub fn init_telemetry() -> Result<()> {
     let resource = Resource::builder().with_service_name("backend").build();
     let endpoint = "http://ft-lgtm-observability:4317";
 
-    logs::init_logs(&resource, endpoint)?;
-    // let log_layer = OpenTelemetryTracingBridge::new(&log_provider);
+    let log_provider = logs::init_logs(&resource, endpoint)?;
+    let log_layer = OpenTelemetryTracingBridge::new(&log_provider);
 
     let tracer_provider = traces::init_traces(&resource, endpoint)?;
     let tracer = tracer_provider.tracer("backend");
@@ -27,7 +28,7 @@ pub fn init_telemetry() -> Result<()> {
 
     tracing_subscriber::registry()
         .with(filter)
-        // .with(log_layer)
+        .with(log_layer)
         .with(trace_layer)
         .with(fmt_layer)
         .init();
